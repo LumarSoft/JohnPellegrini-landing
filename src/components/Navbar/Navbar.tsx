2b7775ae5094d5480632e1f349ce17179b632d84
marcebenitez2 <marcebenitez0607@gmail.com>
2025-01-23 15:23:11 -0300

    feat: los botones de whatsapp e instagram ahora funcionan

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,16 @@
-
 export const Navbar = () => {
   return (
     <div className="h-16 w-full flex justify-between px-4 md:px-28 2xl:px-80 2xl:h-28 items-center py-4 fixed z-20 bg-[#0F0F10] shadow shadow-neutral-800">
-      <a>
-        <img src="/logo_completo.png" className="h-16 w-auto md:h-16 md:py-2 2xl:h-16 hidden lg:block"/>
-        <img src="/logo.png" className="h-10 w-auto md:h-16 md:py-2 2xl:h-24 lg:hidden"/>
+      <a href="/">
+        <img src="/logo_completo.png" className="h-16 w-auto md:h-16 md:py-2 2xl:h-16 hidden lg:block" alt="Logo Completo"/>
+        <img src="/logo.png" className="h-10 w-auto md:h-16 md:py-2 2xl:h-24 lg:hidden" alt="Logo"/>
       </a>
       <div className="flex gap-2 md:gap-4 items-center">
-        <a>
-          <img src="/whatsapp.svg" className="h-10 w-10"/>
+        <a href={`https://wa.me/5493412757294`} target="_blank" rel="noopener noreferrer">
+          <img src="/whatsapp.svg" className="h-10 w-10" alt="WhatsApp"/>
         </a>
-        <a>
-          <img src="/instagram.svg" className="h-10 w-10"/>
+        <a href="https://www.instagram.com/johnpellegriniseguros/" target="_blank" rel="noopener noreferrer">
+          <img src="/instagram.svg" className="h-10 w-10" alt="Instagram"/>
         </a>
       </div>
     </div>
